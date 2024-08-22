a1161f7288d81d5da3d81c600e1911b0f0ca2c24
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-08-22 14:12:50 -0700

    create stat component

--- src/features/dashboard/DashboardLayout.tsx
+++ src/features/dashboard/DashboardLayout.tsx
@@ -1,13 +1,10 @@
 import styled from "styled-components";
-// import { useRecentBookings } from "./useRecentBookings";
 // import Spinner from "../../ui/Spinner";
-// import { useRecentStays } from "./useRecentStays";
-// import Stats from "./Stats";
-// import { useCabins } from "../cabins/useCabins";
 // import SalesChart from "./SalesChart";
 // import DurationChart from "./DurationChart";
 // import TodayActivity from "../check-in-out/TodayActivity";
 import GithubCalendar from "./GithubCalendar";
+import Stats from "./Stats";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -16,17 +13,17 @@ const StyledDashboardLayout = styled.div`
   gap: 2.4rem;
 `;
 function DashboardLayout() {
-  // if (isLoadingBooking || isLoadingStays || isLoadingCabins) return <Spinner />;
+  // if (yearsCoding || numRepos || contributions || languagesUsed) return <Spinner />;
 
   return (
     <StyledDashboardLayout>
-      {/* <Stats
-        bookings={bookings}
-        confirmedStays={confirmedStays}
-        numDays={numDays}
-        cabinCount={cabins.length}
+      <Stats
+        yearsCoding={3}
+        numRepos={67}
+        contributions={442}
+        languagesUsed={6}
       />
-      <TodayActivity />
+      {/* <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} /> */}
       <GithubCalendar />
