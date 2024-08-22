86ca16c621065fe50cf1b3faa466abff846c719f
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-08-22 13:54:33 -0700

    Create header, Spinner and Github calendar component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,14 @@
+import GithubCalendar from "../ui/GithubCalendar";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+      </Row>
+      {/* <DashboardLayout /> */}
+    </>
+  );
 }
---
+++ src/ui/GithubCalendar.tsx
@@ -0,0 +1,12 @@
+import GitHubCalendar from "react-github-calendar";
+
+export default function GithubCalendar() {
+  return (
+    <GitHubCalendar
+      username="soumyajit4419"
+      blockSize={15}
+      blockMargin={5}
+      fontSize={16}
+    />
+  );
+}
