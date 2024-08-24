4a8045a60a222b5daedf7d7e008705b20a39bb68
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-08-24 14:41:31 -0700

    Add pdf files to website - needs additional attention

--- src/features/resume/Resume.tsx
+++ src/features/resume/Resume.tsx
@@ -1,5 +1,22 @@
+import { Document, Page, pdfjs } from "react-pdf";
+import { useEffect, useState } from "react";
 import resumePdf from "../../assets/resume.pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 export default function Resume() {
-  return <embed src={resumePdf} width="100%" height="900vh" />;
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Document file={resumePdf}>
+        <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+      </Document>
+    </div>
+  );
 }
