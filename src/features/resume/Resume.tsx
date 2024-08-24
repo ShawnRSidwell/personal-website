6b0dc7518ad7b88b0ad47f0eef123ed97cff89db
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-08-24 14:58:01 -0700

    Add resume to resume page

--- src/features/resume/Resume.tsx
+++ src/features/resume/Resume.tsx
@@ -2,6 +2,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import { useEffect, useState } from "react";
 import resumePdf from "../../assets/resume.pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
