851b6a1357f362c7800cd6751debfa5128f0ab51
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-08-26 12:42:51 -0700

    Add loading spinner to resume

--- src/features/resume/Resume.tsx
+++ src/features/resume/Resume.tsx
@@ -3,11 +3,18 @@ import { useEffect, useState } from "react";
 import resumePdf from "../../assets/resume.pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
+import Spinner from "../../ui/Spinner";
+import styled from "styled-components";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
+const LoadingMessage = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 export default function Resume() {
   const [width, setWidth] = useState(1200);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -15,7 +22,13 @@ export default function Resume() {
 
   return (
     <div>
-      <Document file={resumePdf}>
+      {isLoading && <Spinner />}
+      <Document
+        file={resumePdf}
+        onLoadSuccess={() => setIsLoading(false)}
+        onLoadError={() => setIsLoading(false)}
+        loading={<LoadingMessage>Loading Pdf...</LoadingMessage>}
+      >
         <Page
           pageNumber={1}
           scale={width > 786 ? 1.7 : 0.6}
